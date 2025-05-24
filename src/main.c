cdce53d5ae2ce7b19f8ec31bc1ae31735f2bdb6b
Luis Rodrigues <luisr.silva@outlook.com>
2025-05-23 23:55:56 -0300

    fix: Fixing the function call

--- src/main.c
+++ src/main.c
@@ -2,9 +2,9 @@
 
 int main(void) {
   BstPtr tree = newBinarySearchTree(10);
-  insert(tree, 9);
-  insert(tree, 11);
-  insert(tree, 12);
-  insert(tree, 1);
+  insertKey(tree, 9);
+  insertKey(tree, 11);
+  insertKey(tree, 12);
+  insertKey(tree, 1);
   return 0;
 }
