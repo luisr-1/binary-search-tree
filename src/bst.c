d5fae9ea14189a89b4fa530718dd3a1e56338028
Luis Rodrigues <luisr.silva@outlook.com>
2025-05-23 23:30:50 -0300

    feat: implementing newNode and insertKey operation

--- src/bst.c
+++ src/bst.c
@@ -1,36 +1,36 @@
-//
-// Created by luis on 21/05/25.
-//
-
 #include "bst.h"
-
 #include <stdlib.h>
 
 BstPtr newBinarySearchTree(int key) {
-    struct bst *tree = malloc(sizeof(struct bst));
-    tree->root = malloc(sizeof(struct node));
-    tree->root->left = NULL;
-    tree->root->right = NULL;
-    tree->root->parent = NULL;
-    tree->root->data = key;
-    return tree;
+  BstPtr tree = malloc(sizeof(struct bst));
+  tree->root = newNode(key);
+  return tree;
 }
 
-void insert(BstPtr tree, int key) {
-    NodePtr current = tree->root;
+void insertKey(BstPtr tree, int key) {
+  NodePtr current = tree->root;
+  NodePtr node = newNode(key);
 
-    if(current->data < key) {
-        while(current->right != NULL)
-            current = current->right;
+  if (current->data <= key) {
+    while (current->right != NULL)
+      current = current->right;
 
-        if (current->right == NULL) {
-            NodePtr node = malloc(sizeof(struct node));
-            node->right = NULL;
-            node->left = NULL;
-            node->parent = current;
-            node->data = key;
-            current->right = node;
-            return;
-        }
-    }
+    node->parent = current;
+    current->right = node;
+    return;
+  }
+
+  while (current->left != NULL)
+    current = current->left;
+
+  node->parent = current;
+  current->left = node;
+}
+
+NodePtr newNode(int data) {
+  NodePtr newNode = malloc(sizeof(struct node));
+  newNode->data = data;
+  newNode->right = NULL;
+  newNode->left = NULL;
+  return newNode;
 }
